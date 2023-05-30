426a38d5bd193c017f61d737524bdda0eeed143a
hernan gustavo quiroga <desarrolladoryotti@gmail.com>
2023-05-30 16:17:56 -0300

    cualquiercosa

---
+++ app.js
@@ -0,0 +1,11 @@
+console.log("hola mundo");
+
+let edad = prompt("ingrese edad");
+
+if (edad == 18 || edad < 18) {
+
+    console.log("es mayor de edad")
+} else {
+    console.log("es maenor de edad");
+}
+console.log("mi nombres ", nombre)
